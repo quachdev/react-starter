92eb2d9964d78f034049220eb34f9f032ab9dda4
Simon Quach <quach.dev@gmail.com>
2016-02-18 22:06:46 -0500

    Initialize React Webpack starter

---
+++ app/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Note from './Note.js';
+
+export default class App extends React.Component {
+  render() {
+    return <Note />;
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,6 @@
+require('./main.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
